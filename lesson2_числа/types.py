ea68f4a66a98ad08fe76979301c727f80c5cb9bf
Dmitriy <dmitriy250300@mail.ru>
2023-10-02 16:16:32 +0300

    01.10.2023

---
+++ lesson2_числа/types.py
@@ -0,0 +1,28 @@
+# целые числа:
+digit1 = 10
+digit2 = 5*3
+
+# десятичные дроби:
+fl1 = 5.23
+fl2 = 3 * 1.1
+
+# строки
+text1 = 'Hello!'
+text2 = 5 * ' name '
+
+
+print(text2)
+print(type(text2))
+
+'''
+int
+str
+float
+bool
+
+list
+tuple
+set
+frozenset
+dict
+'''
