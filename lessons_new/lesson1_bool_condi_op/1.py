3878984d27a8ca45ec0bd06dfc6d123a046b58e5
Dmitriy <dmitriy250300@mail.ru>
2023-11-19 16:23:47 +0300

    19.11.2023

---
+++ lessons_new/lesson1_bool_condi_op/1.py
@@ -0,0 +1,15 @@
+result1 = False
+result2 = True
+result3 = 5 > 2
+result4 = 5 < 3
+result5 = 2 == 2
+result6 = 1 != 9
+result7 = 20 <= 10
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+print(result5)
+print(result6)
+print(result7)
