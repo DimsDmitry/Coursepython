06fefa01563665cc16029b6a3d34d135046974c3
Dmitriy <dmitriy250300@mail.ru>
2024-11-23 16:06:38 +0300

    23.11.2024

--- Ivan/lesson_part5_OOP/lesson3_event_catch_turtle/catch_turtle.py
+++ Ivan/lesson_part5_OOP/lesson3_event_catch_turtle/catch_turtle.py
@@ -13,8 +13,9 @@ w = 200
 h = 200
 
 
+# функции, отвечающие за поимку черепашек
 def catch1(x, y):
-    # черепашка 1 поймана
+    """черепашка 1 поймана"""
     t1.penup()
     t1.goto(randint(-100, 100), randint(-100, 100))
     t1.pendown()
@@ -22,7 +23,7 @@ def catch1(x, y):
 
 
 def catch2(x, y):
-    # черепашка 2 поймана
+    """черепашка 2 поймана"""
     t2.penup()
     t2.goto(randint(-100, 100), randint(-100, 100))
     t2.pendown()
@@ -30,50 +31,56 @@ def catch2(x, y):
 
 
 def catch3(x, y):
-    # черепашка 3 поймана
+    """черепашка 3 поймана"""
     t3.penup()
     t3.goto(randint(-100, 100), randint(-100, 100))
     t3.pendown()
     t3.left(randint(0, 180))
 
 
+def divide():
+    """рисует рамку для игрового поля"""
+    t4 = Turtle()
+    t4.speed(0)
+    t4.pensize(3)
+    t4.penup()
+    t4.goto(-200, -200)
+    t4.pendown()
+    for i in range(4):
+        t4.forward(400)
+        t4.left(90)
+    t4.hideturtle()
+
+
 def stop_game(t1, t2, t3):
-    # проверяем, не достигла ли одна из черепашек края игрового окна
+    """возвращает False, если все черепашки в границах окна, True - если одна из черепашек достигла их"""
     t1_out = abs(t1.xcor()) > w or abs(t1.ycor()) > h
     t2_out = abs(t2.xcor()) > w or abs(t2.ycor()) > h
     t3_out = abs(t3.xcor()) > w or abs(t3.ycor()) > h
     out = t1_out or t2_out or t3_out
     return out
 
-
-def divide():
-    # рисует рамку для игрового поля
-    t4 = Turtle()
-    t4.pensize(5)
-    t4.penup()
-    t4.goto(-200, 200)
-    t4.pendown()
-    for i in range(4):
-        t4.forward(400)
-        t4.right(90)
-
-
-divide()
 # создаём черепашек
 t1 = Turtle()
-t1.color('blue')
+t1.color('green')
 t1.shape('turtle')
 
 t2 = Turtle()
-t2.color('green')
-t2.left(120)
+t2.color('red')
 t2.shape('turtle')
+t2.left(120)
 
 t3 = Turtle()
-t3.color('red')
-t3.left(-120)
+t3.color('blue')
 t3.shape('turtle')
+t3.left(-120)
 
+sleep(1)
+
+divide()
+
+sleep(1)
+# подключаем обработку событий
 t1.onclick(catch1)
 t2.onclick(catch2)
 t3.onclick(catch3)
@@ -84,10 +91,4 @@ while not stop_game(t1, t2, t3):
     t3.forward(7)
     sleep(0.1)
 
-sleep(3)
-t1.clear()
-t2.clear()
-t3.clear()
-
-write('Игра окончена!', font=('Arial', 25, 'normal'))
 exitonclick()
